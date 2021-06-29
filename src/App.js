9b38aa480540461e4d7579cd7ca85970ffa8c912
vamigoya97 <amigo.victor.97@gmail.com>
2021-06-29 19:14:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaItalic } from 'react-icons/fa';
+import { FaWeebly } from 'react-icons/fa';
+import { FaLemon } from 'react-icons/fa';
+import { FaArtstation } from 'react-icons/fa';
+import { FaTheaterMasks } from 'react-icons/fa';
+import { FaPuzzlePiece } from 'react-icons/fa';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='info-container'>
+          <div>
+            <p>HOW DO I KNOW WHICH LOGO STYLE IS RIGHT FOR ME?</p>
+            <h2>Different Types of Logo Styles</h2>
+          </div>
+          <div>
+            <p>Contact Us</p>
+          </div>
+        </div>
+        <div className='options-container'>
+            <div className='option-card'>
+              <FaItalic />
+              <p>Lettermark Logos</p>
+            </div>
+            <div className='option-card'>
+              <FaWeebly />
+              <p>Wordmark Logos</p>
+            </div>
+            <div className='option-card'>
+              <FaLemon />
+              <p>Pictorial Mark Logos</p>
+            </div>
+            <div className='option-card'>
+              <FaArtstation />
+              <p>Abstract Mark Logos</p>
+            </div>
+            <div className='option-card'>
+              <FaTheaterMasks />
+              <p>Mascot Logos</p>
+            </div>
+            <div className='option-card'>
+              <FaPuzzlePiece />
+              <p>Combination Logos</p>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
